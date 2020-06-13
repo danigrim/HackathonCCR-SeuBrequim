3a3d4aa18493606f75a6360da2b90a255a254e0e
GabrielLima1995 <gabriel-souzalima@live.com>
2020-06-13 18:27:53 -0300

    criacao_funcao_main

--- maps/main.py
+++ maps/main.py
@@ -6,34 +6,37 @@ API_key = 'AIzaSyDVyd-mpP6ugSDfe0AmRaVusa7BUCp-A5o'
 origem = 'rodovia presidente castelo branco km 300'
 url = "https://maps.googleapis.com/maps/api/staticmap?"
 icon_url ='https://www.muygle.com/node-red/muygle/api/language?name=police'
-lugares  = 'police'
+lugares  = ['police','supermarket']
 
-# Google client
-gmaps = googlemaps.Client(key = API_key)
+def main():
 
-# Geocoding convert
-a =geocoding.convert()
-lat,lng  = a.logradouro_para_coordenadas(origem)
+    # Google client
+    gmaps = googlemaps.Client(key = API_key)
 
-# Encontrando Lugares próximos
-places_results = gmaps.places_nearby(location = (lat,lng),radius = 2000,open_now = False ,
-                                     type =lugares)
+    # Geocoding convert
+    a =geocoding.convert()
+    lat,lng  = a.logradouro_para_coordenadas(origem)
 
-list = places_results['results']
-pins =''
-for i in list:
-    name     =  i['name']
-    lat_long =  i['geometry']['location']
-    lat      = str(lat_long['lat'])
-    long     = str(lat_long['lng'])
-    local    = str(lat+','+long)
-    pins    += '|'+local
+    # Encontrando Lugares próximos
+    places_results = gmaps.places_nearby(location = (lat,lng),radius = 2000,open_now = False ,
+                                         type =lugares)
 
-#Gerando o mapa
-r = requests.get(url+'center='+origem+'&zoom=15&format=jpg&size=600x600&markers=anchor:bottomright|'
-                                      'icon:'+icon_url+pins+'&key='+API_key)
+    list = places_results['results']
+    pins =''
+    for i in list:
+        name     =  i['name']
+        lat_long =  i['geometry']['location']
+        lat      = str(lat_long['lat'])
+        long     = str(lat_long['lng'])
+        local    = str(lat+','+long)
+        pins    += '|'+local
+
+    #Gerando o mapa
+    r = requests.get(url+'center='+origem+'&zoom=15&format=jpg&size=600x600&markers=anchor:bottomright|'
+                                          'icon:'+icon_url+pins+'&key='+API_key)
 
 
-f = open('seubrequin.jpg', 'wb')
-f.write(r.content)
-f.close()
+    f = open('seubrequin.jpg', 'wb')
+    f.write(r.content)
+    f.close()
+    return 0
